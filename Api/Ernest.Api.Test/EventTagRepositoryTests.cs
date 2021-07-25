9fd1ca3a769955d2a9e3992d95f7ebcf9d0dc745
Josh Perry <joshperry@outlook.com>
2021-07-25 12:23:27 +0100

    ApiTest: added a couple more event tag tests

--- Api/Ernest.Api.Test/EventTagRepositoryTests.cs
+++ Api/Ernest.Api.Test/EventTagRepositoryTests.cs
@@ -7,7 +7,7 @@ namespace Ernest.Api.Test
     public class EventTagRepositoryTests
     {
         [Fact]
-        public void Test_EventTagRepository_GetByName_GetsTag()
+        public void Test_EventTagRepository_ExistentGetByName_GetsTag()
         {
             // Arrange
             var dbContext = TestDbContextSeeder.InitializeDatabase();
@@ -19,6 +19,42 @@ namespace Ernest.Api.Test
             // Assert
             Assert.NotNull(tag);
             Assert.True(tag.Title == "TestTag");
+        }
+
+        [Fact]
+        public void Test_EventTagRepository_NonExistentGetByName_GetsNull()
+        {
+            // Arrange
+            var dbContext = TestDbContextSeeder.InitializeDatabase();
+            var eventTagRepository = new EventTagsRepository(dbContext);
+
+            // Act
+            var tag = eventTagRepository.GetByName("TagThatDoesn'tExist").FirstOrDefault();
+
+            // Assert
+            Assert.Null(tag);
+        }
+
+        [Fact]
+        public void Test_EventTagRepository_GetByNamesArray_GetsCorrectTags()
+        {
+            // Arrange
+            var dbContext = TestDbContextSeeder.InitializeDatabase();
+            var eventTagRepository = new EventTagsRepository(dbContext);
+
+            // Act
+            var tags = eventTagRepository.GetByName(new[]
+            {
+                "TestTag",
+                "TestTag2",
+                "TestTag3"
+            }).ToList();
+
+            // Assert
+            Assert.True(tags.Count == 3);
+            Assert.True(tags.Count(x => x.Title == "TestTag") == 1);
+            Assert.True(tags.Count(x => x.Title == "TestTag2") == 1);
+            Assert.True(tags.Count(x => x.Title == "TestTag3") == 1);
         }
     }
 }
