a9f72a3e7a8ec3ebe61db00b69644ba9db3efc1a
Josh Perry <joshperry@outlook.com>
2021-07-25 12:42:18 +0100

    Test: scaffold some event types

--- Api/Ernest.Api.Test/SetupUtilities/TestDbContextSeeder.cs
+++ Api/Ernest.Api.Test/SetupUtilities/TestDbContextSeeder.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Ernest.Api.Data;
 using Ernest.Api.Models.Db;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,7 @@ namespace Ernest.Api.Test.SetupUtilities
             dbContext.Database.EnsureDeleted();
 
             AddEventTags(dbContext);
+            AddEventTypes(dbContext);
             return dbContext;
         }
 
@@ -39,6 +41,67 @@ namespace Ernest.Api.Test.SetupUtilities
                 });
             }
 
+            dbContext.SaveChanges();
+        }
+
+        private static void AddEventTypes(ApplicationDbContext dbContext)
+        {
+            var eventTypes = new List<EventType>
+            {
+                new()
+                {
+                    Title = "Tea"
+                },
+
+                new()
+                {
+                    Title = "Weigh-in",
+                    DecimalFields = new List<EventDecimalFieldTemplate>
+                    {
+                        new()
+                        {
+                            Title = "Weight (lb)",
+                            Priority = 1
+                        }
+                    }
+                },
+
+                new()
+                {
+                    Title = "Ideas",
+                    StringFields = new List<EventStringFieldTemplate>
+                    {
+                        new()
+                        {
+                            Title = "Idea",
+                            Priority = 1
+                        }
+                    }
+                },
+
+                new()
+                {
+                    Title = "Mood",
+                    IntegerFields = new List<EventIntegerFieldTemplate>
+                    {
+                        new()
+                        {
+                            Title = "Mood",
+                            Priority = 1
+                        }
+                    },
+                    StringFields = new List<EventStringFieldTemplate>
+                    {
+                        new()
+                        {
+                            Title = "Notes",
+                            Priority = 2
+                        }
+                    }
+                }
+            };
+
+            dbContext.AddRange(eventTypes);
             dbContext.SaveChanges();
         }
     }
